1d1df00abaa14192ee3c1244fb971e8ecc8d6ce6
Madeleine Mihailides <madeleine@Mihailides.com>
2019-04-27 17:26:29 +1000

    deleted camera, started working on overlay

---
+++ Bread/Assets/Scripts/Overlay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Overlay : MonoBehaviour
+{
+    private float timer;
+    public Image image;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        timer = 0.0f;
+    }
+
+    void Update()
+    {
+        timer = Time.deltaTime;
+        if (timer > 1) 
+        {
+            ScreenFlash();
+            timer -= 1;
+        }
+    }
+
+    private void ScreenFlash() 
+    {   
+
+    }
+}
