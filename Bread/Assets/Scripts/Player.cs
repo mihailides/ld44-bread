5bf15e0467045adae8fd3a47180cd030f2eef516
Dylan Jenkins <dylanljenkins@gmail.com>
2019-04-27 21:59:48 +1000

    Added sprites

--- Bread/Assets/Scripts/Player.cs
+++ Bread/Assets/Scripts/Player.cs
@@ -29,10 +29,15 @@ public class Player : MonoBehaviour
         Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0);
         
         var moving = (Math.Abs(moveHorizontal) > 0.1 || Math.Abs(moveVertical) > 0.1);
+        if (moving)
+        {
+            var angle = Mathf.Atan2(moveHorizontal, moveVertical) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.back);
+        }
+
         animator.SetBool("moving", moving);
         
         transform.position += movement * speed * Time.deltaTime;
         // transform.Translate(moveHorizontal * speed, moveVertical * speed, 0);
-
     }
 }
