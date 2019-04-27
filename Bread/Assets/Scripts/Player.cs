f1f977a6eed65968cf17dee0a8cc4bcac26915ed
Dylan Jenkins <dylanljenkins@gmail.com>
2019-04-27 21:39:56 +1000

    Player animation

--- Bread/Assets/Scripts/Player.cs
+++ Bread/Assets/Scripts/Player.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;            
+    public float speed;
+    public Animator animator;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -24,6 +27,9 @@ public class Player : MonoBehaviour
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
         Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0);
+        
+        var moving = (Math.Abs(moveHorizontal) > 0.1 || Math.Abs(moveVertical) > 0.1);
+        animator.SetBool("moving", moving);
         
         transform.position += movement * speed * Time.deltaTime;
         // transform.Translate(moveHorizontal * speed, moveVertical * speed, 0);
