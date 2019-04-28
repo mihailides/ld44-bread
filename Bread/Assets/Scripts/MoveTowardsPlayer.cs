bf55634461fe898733f34f968b966cad6706ea7c
Dylan Jenkins <dylanljenkins@gmail.com>
2019-04-28 18:54:53 +1000

    Updated pathfinding.

--- Bread/Assets/Scripts/MoveTowardsPlayer.cs
+++ Bread/Assets/Scripts/MoveTowardsPlayer.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class MoveTowardsPlayer : MonoBehaviour
 {
     public Component player;
-
+    public float speed;
+    
     private AStarPathfinding aStarPathfinding;
     
     // Start is called before the first frame update
@@ -17,6 +18,27 @@ public class MoveTowardsPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        aStarPathfinding.FindPath(transform.position, player.transform.position); 
+        var findPath = aStarPathfinding.FindPath(transform.position, player.transform.position);
+
+        // Nowhere to path-find.
+        if (findPath.Count == 0) return;
+        
+        // Find the next cell to move into.
+        var worldPoint = aStarPathfinding.WorldPointFromNode(findPath[0]);
+        var target = worldPoint - transform.position;
+
+        // If the next cell is too close and there are more cells, move towards the second-closest cell.
+        if (target.magnitude < 1 && findPath.Count > 1)
+        {
+            worldPoint = aStarPathfinding.WorldPointFromNode(findPath[1]);
+            target = worldPoint - transform.position;
+        }
+            
+        // Rotate towards next cell.
+        transform.up = target;
+            
+        // Move towards next cell.
+        var movement = Vector3.ClampMagnitude(target, 1);
+        transform.position += movement * speed * Time.deltaTime;
     }
 }
